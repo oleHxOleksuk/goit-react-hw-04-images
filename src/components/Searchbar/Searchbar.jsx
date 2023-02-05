ffe55b587d0a0fd5ada92d0edcd016b84ccaf5e7
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-02-05 23:07:07 +0200

    fvdfbdn

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,46 @@
-import {Component} from "react";
+import {useState} from "react";
+import PropTypes from "prop-types";
 import styles from './search-bar.module.scss'
-class Searchbar extends Component {
-  state = {
-    search:'',
-  }
-  handleChange = ({target}) =>{
-    const {name, value} = target;
-    this.setState ({[name]: value});
-  };
-  handleSubmit = e =>{
-    e.preventDefault()
-    const {onSubmit} = this.props
-    onSubmit(this.state.search)
-  }
-  reset(){
-    this.setState({
-      search:'',
-    })
-  }
-  render() {
-    const { search } = this.state;
-    const { handleChange, handleSubmit } = this;
-    return(
-      <form className={styles.Searchbar} onSubmit={handleSubmit}>
-        <div className={styles.SearchForm}>
-          <button type="submit" className={styles.SearchFormButton}>
-            <div className={styles.SearchForButtonLabel}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
-              </svg>
-            </div>
-          </button>
 
-          <input
-            className={styles.SearchForInput}
-            value={search}
-            onChange={handleChange}
-            name="search"
-            placeholder="Search images"
-            required
-          />
-        </div>
-      </form>
-    )
-  }
+const Searchbar = ({onSubmit}) => {
+  const [state, setState] = useState("")
+
+  const handleChange = ({target}) =>{
+    const {value} = target;
+    setState (value);
+  };
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+    onSubmit(state)
+    setState("");
+  };
+
+  return(
+    <form className={styles.Searchbar} onSubmit={handleSubmit}>
+      <div className={styles.SearchForm}>
+        <button type="submit" className={styles.SearchFormButton}>
+          <div className={styles.SearchForButtonLabel}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
+            </svg>
+          </div>
+        </button>
+
+        <input
+          className={styles.SearchForInput}
+          value={state}
+          onChange={handleChange}
+          name="search"
+          placeholder="Search images"
+          required
+        />
+      </div>
+    </form>
+  )
 }
-export default Searchbar
+export default Searchbar;
+
+Searchbar.protoTypes = {
+  onSubmit: PropTypes.func.isRequired,
+}
